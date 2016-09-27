4adad6dcf38b7a75fa8d17f9a6a78e9da5d0343e
RupaliDeshmane <rohinideshmane.21@gmail.com>
2016-09-27 17:55:06 +0530

    Some changes related to error solving in async task loader

--- app/src/main/java/com/example/android/newsapp/MainActivity.java
+++ app/src/main/java/com/example/android/newsapp/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.android.newsapp;
 import android.app.LoaderManager;
 import android.content.AsyncTaskLoader;
 import android.content.Context;
-import android.content.Loader;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -28,9 +26,10 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<newsInfo>> {
+public class MainActivity extends AppCompatActivity {
 
     EditText searchString;
     Button btnSearch;
@@ -44,43 +43,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
      * This really only comes into play if you're using multiple loaders.
      */
     private int EARTHQUAKE_LOADER_ID = 1;
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
-    private String API_URL ="http://content.guardianapis.com/search?q=android&api-key=test&show-tags=contributor";
-    @Override
-    public Loader<ArrayList<newsInfo>> onCreateLoader(int i, Bundle bundle) {
-        // Create a new loader for the given URL
-
-        return (new HttpAsyncTaskLoader(this,API_URL));
-
-    }
-
-    @Override
-    public void onLoadFinished(Loader<ArrayList<newsInfo>> loader, ArrayList<newsInfo> data) {
-        // Clear the adapter of previous earthquake data
-        newsAdapter.clear();
-        newslist = data;
-        // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
-        // data set. This will trigger the ListView to update.
-        if (data != null && !data.isEmpty()) {
-            //newsAdapter.addAll(data);
-            showData(data);
-        }
-
-    }
-
-    @Override
-    public void onLoaderReset(Loader<ArrayList<newsInfo>> loader) {
-        // Loader reset, so we can clear out our existing data.
-       // newsAdapter.clear();
-       showData(newslist);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         // Get a reference to the LoaderManager, in order to interact with loaders.
         LoaderManager loaderManager = getLoaderManager();
@@ -88,7 +57,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         // Initialize the loader. Pass in the int ID constant defined above and pass in null for
         // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
         // because this activity implements the LoaderCallbacks interface).
-        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+       // loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null,null);
 
         btnSearch = (Button)findViewById(R.id.buttonSearch);
         msg = (TextView) findViewById(R.id.textViewMsg);
@@ -104,38 +73,25 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         final String enteredText = "";
 
-       /* btnSearch.setOnClickListener(new View.OnClickListener() {
+        btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 progress.setVisibility(View.VISIBLE);
                 progress.setProgress(0);
                 msg.setVisibility(View.GONE);
 
-
-               /* if (!newslist.isEmpty()) {
-                    newslist.clear();
-                }
+                String enteredText = null;
                 try {
-                    String enteredText = URLEncoder.encode(searchString.getText().toString(), "utf-8");
-
+                    enteredText = URLEncoder.encode(searchString.getText().toString(), "utf-8");
                     String url = "http://content.guardianapis.com/search?q=" + enteredText + "&api-key=test&show-tags=contributor";//https://www.googleapis.com/books/v1/volumes?q=" + enteredText + "&maxResults=10";
 
-                    ConnectivityManager cm =
-                            (ConnectivityManager) MainActivity.this.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-                    NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-                    boolean isConnected = activeNetwork != null &&
-                            activeNetwork.isConnectedOrConnecting();
-
-                    if (isConnected == true) {
-                      //  new HttpAsyncTask().execute(url);
-                    }
-
+                    HttpAsyncTaskLoader loader = new HttpAsyncTaskLoader(MainActivity.this, url);
+                    loader.startLoading();
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
-                }*/
-            /*}
-        });*/
+                }
+            }
+        });
     }
 
     public void showData(ArrayList<newsInfo> newsInfo)
@@ -193,16 +149,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         return total.toString();
     }
 
-   /* private static String convertInputStreamToString(InputStream inputStream) throws IOException{
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while((line = bufferedReader.readLine()) != null)
-            result += line;
-        inputStream.close();
-        return result;
-    }*/
-
     public class HttpAsyncTaskLoader extends AsyncTaskLoader<ArrayList<newsInfo>>
     {
         private String murl;
@@ -246,52 +192,28 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                     }
                     newslist.add(new newsInfo(title,type,webUrl,contributorName));
                 }
+                //showData(newslist);
 
             } catch (JSONException e) {
                 e.printStackTrace();
             }
             return newslist;
         }
-    }
-    /*private class HttpAsyncTask extends AsyncTask<String, Void, String> {
-        String contributorName = "";
 
         @Override
-        protected String doInBackground(String... urls) {
-
-            String result = GET(urls[0]);
-
-            JSONObject jsonObject = null;
-            try {
-                jsonObject = new JSONObject(result);
-                JSONArray results = jsonObject.getJSONObject("response").getJSONArray("results");
-                for(int i=0; i< results.length();i++)
-                {
-                    JSONObject news = results.getJSONObject(i);
-                    String title = news.get("webTitle").toString();
-                    String type = news.get("type").toString();
-                    String webUrl = news.get("webUrl").toString();
-
-                    if(type.equals("contributor"))
-                    {
-                        contributorName = news.get("firstName")+" "+news.get("lastName");
-                    }
-                    newslist.add(new newsInfo(title,type,webUrl,contributorName));
-                }
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return GET(urls[0]);
-        }
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String result) {
-            // Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
-            showData(newslist);
-            msg.setVisibility(View.GONE);
+        public void deliverResult(ArrayList<newsInfo> data) {
+            super.deliverResult(data);
             progress.setVisibility(View.GONE);
+            progress.setProgress(0);
             msg.setVisibility(View.GONE);
+            showData(newslist);
         }
-    }*/
+
+        @Override
+        protected void onReset() {
+            super.onReset();
+            showData(newslist);
+        }
+
+    }
 }
